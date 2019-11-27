362bccf8197fe57649fd59d961ba8ee803932c37
miaow <1772256025@qq.com>
2019-11-27 14:49:11 +0800

    修改测试用例的数据传递方式为参数化

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,6 +1,8 @@
 """
 登录测试用例
 """
+import pytest
+
 from common.utils import init_driver
 from page.page_factory import PageFactory
 
@@ -15,14 +17,15 @@ class TestLogin(object):
     def teardown_class(self):
         self.driver.quit()  # 退出驱动对象
 
-    def test_login(self):
+    @pytest.mark.parametrize('name,pwd,expect', [('18132519715', '999999', '8189')])
+    def test_login(self, name, pwd, expect):
         """登录测试方法"""
-        self.page_factory.home_page.click_mine()# 点击我的
-        self.page_factory.mine_page.click_login()# 点击登录注册
-        self.page_factory.login_page.input_username('18132519715') #输入用户名
-        self.page_factory.login_page.input_password('999999')#输入密码
-        self.page_factory.login_page.click_login_btn()#点击登录按钮
-        self.page_factory.login_page.click_con_btn()#点击签到确认按钮
-        nick_name = self.page_factory.login_page.get_nick_name()#获取昵称
-        print('昵称是：',nick_name)
-        assert '8189' in nick_name #断言判断结果
+        self.page_factory.home_page.click_mine()  # 点击我的
+        self.page_factory.mine_page.click_login()  # 点击登录注册
+        self.page_factory.login_page.input_username(name)  # 输入用户名
+        self.page_factory.login_page.input_password(pwd)  # 输入密码
+        self.page_factory.login_page.click_login_btn()  # 点击登录按钮
+        self.page_factory.login_page.click_con_btn()  # 点击签到确认按钮
+        nick_name = self.page_factory.login_page.get_nick_name()  # 获取昵称
+        print('昵称是：', nick_name)
+        assert expect in nick_name  # 断言判断结果
